9a5a4ffaf771fc8f5faf13d28dd24645a0cdabb8
SongKJ00 <gowithdsm@gmail.com>
2022-03-03 14:38:55 +0900

    day2 문제 초기 해결

--- clojure/src/aoc2018_2.clj
+++ clojure/src/aoc2018_2.clj
@@ -1,37 +1,69 @@
-(ns aoc2018-2)
+(ns aoc2018-2
+  (:require [clojure.java.io :as io] [clojure.string :as str]))
 
-;; 파트 1
-;; 주어진 각각의 문자열에서, 같은 문자가 두번 혹은 세번씩 나타난다면 각각을 한번씩 센다.
-;; 두번 나타난 문자가 있는 문자열의 수 * 세번 나타난 문자가 있는 문자열의 수를 반환하시오.
-;; 예)
-;; abcdef 어떤 문자도 두번 혹은 세번 나타나지 않음 -> (두번 나오는 문자열 수: 0, 세번 나오는 문자열 수: 0)
-;; bababc 2개의 a, 3개의 b -> (두번 나오는 문자열 수: 1, 세번 나오는 문자열 수: 1)
-;; abbcde 2개의 b -> (두번 나오는 문자열 수: 2, 세번 나오는 문자열 수: 1)
-;; abcccd 3개의 c -> (두번 나오는 문자열 수: 2, 세번 나오는 문자열 수: 2)
-;; aabcdd 2개의 a, 2개의 d 이지만, 한 문자열에서 같은 갯수는 한번만 카운트함 -> (두번 나오는 문자열 수: 3, 세번 나오는 문자열 수: 2)
-;; abcdee 2개의 e -> (두번 나오는 문자열 수: 4, 세번 나오는 문자열 수: 2)
-;; ababab 3개의 a, 3개의 b 지만 한 문자열에서 같은 갯수는 한번만 카운트함 -> (두번 나오는 문자열 수: 4, 세번 나오는 문자열 수: 3)
-;; 답 : 4 * 3 = 12
+;; part1
+(defn get-input-puzzle
+  [filename]
+  (let [input-file (io/resource filename)]
+    (str/split (slurp input-file) #"\n")))
 
+(defn get-frequencies-set
+  [box-id]
+  (->> box-id
+       frequencies
+       vals
+       set))
 
-;; 파트 2
-;; 여러개의 문자열 중, 같은 위치에 정확히 하나의 문자가 다른 문자열 쌍에서 같은 부분만을 리턴하시오.
-;; 예)
-;; abcde
-;; fghij
-;; klmno
-;; pqrst
-;; fguij
-;; axcye
-;; wvxyz
+(defn contains-frequency
+  [frequencies-set frequency]
+  (if (frequencies-set frequency) 1 0))
 
-;; 주어진 예시에서 fguij와 fghij는 같은 위치 (2번째 인덱스)에 정확히 한 문자 (u와 h)가 다름. 따라서 같은 부분인 fgij를 리턴하면 됨.
+(defn get-frequencies-info
+  [frequencies frequencies-set]
+  (map (partial contains-frequency frequencies-set) frequencies))
 
+;; (defn get-frequencies-info
+;;   [frequencies frequencies-set]
+;;   (list
+;;    contains-frequency frequencies-set 2
+;;    contains-frequency frequencies-set 3))
 
-;; #################################
-;; ###        Refactoring        ###
-;; #################################
+(comment
+  (->> (get-input-puzzle "day2.sample.txt")
+       (map get-frequencies-set)
+       (map (partial get-frequencies-info [2 3]))
+       (reduce (fn [x y] (map + x y)))
+       (apply *)))
 
-;; frequencies 사용하기
-;; PPAP (parse-process-aggregate-print) 원칙 따르기
-;; declarative 한 함수 이름 사용하기
+;; part2
+(defn diff-only-one-letter
+  [[s1 s2]]
+  (->> (map not= s1 s2)
+       (filter true?)
+       count
+       (= 1)))
+
+(defn get-same-part
+  [[s1 s2]]
+  (loop [idx 0
+         max-length (count s1)
+         same-part ""]
+    (if (< idx max-length)
+      (if (= (nth s1 idx) (nth s2 idx))
+        (recur (+ idx 1) max-length (str same-part (nth s1 idx)))
+        (recur (+ idx 1) max-length same-part))
+      same-part)))
+
+(defn get-combination
+  [box-ids]
+  (for [x box-ids
+        y box-ids
+        :when (not= x y)]
+    (vector x y)))
+
+(comment
+  (->> (get-input-puzzle "day2.sample.txt")
+       (get-combination)
+       (filter diff-only-one-letter)
+       (map get-same-part)
+       first))
