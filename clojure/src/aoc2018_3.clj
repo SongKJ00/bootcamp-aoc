dc6a7d825f596c39054acdb6d5e7043eab167a91
SongKJ00 <gowithdsm@gmail.com>
2022-03-04 19:11:51 +0900

    day3) 리뷰 반영 리팩토링

--- clojure/src/aoc2018_3.clj
+++ clojure/src/aoc2018_3.clj
@@ -1,5 +1,7 @@
 (ns aoc2018_3
-  (:require [clojure.java.io :as io] [clojure.string :as str]))
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]
+            [clojure.set :refer [intersection]]))
 
 (defn get-input-puzzle [filename]
   (-> filename
@@ -13,46 +15,38 @@
    output: [947, 775, 93, 16, 14]"
   [input]
   (->> input
-       (re-find #"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)")
+       (re-matches #"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)")
        rest
        (map #(Integer/parseInt %))))
 
 (defn get-claim
   "파싱한 인풋으로 claim map 생성
    input: [947, 775, 93, 16, 14]
-   output: {:id 947 :start-x 775 :start-y 93 :len-x 16 :len-y 14}"
-  [[id start-x start-y len-x len-y]]
-  {:id id
-   :start-x start-x
-   :start-y start-y
-   :len-x len-x
-   :len-y len-y})
-
-;; (defn parse-inputs
-;;   [inputs]
-;;   (let [blocks (map #(Integer/parseInt %) (drop 1 (str/split inputs #"#|( @ )|,|: |x")))] 
-;;     {:id (nth blocks 0),
-;;      :start-x (nth blocks 1),
-;;      :start-y (nth blocks 2),
-;;      :len-x (nth blocks 3),
-;;      :len-y (nth blocks 4)}))
+   output: {:id 947 :x 775 :y 93 :w 16 :h 14}"
+  [[id x y w h]]
+   {:id id
+   :x x
+   :y y
+   :w w
+   :h h})
 
 ;; part 1
 (defn get-coords
   "claim에서 좌표 벡터 생성
-   input: {:start-x 1 :start-y 1 :len-x 2 :len-y 2}
+   input: {:x 1 :y 1 :w 2 :h 2}
    output: [[1 1] [1 2] [2 1] [2 2]]"
-  [{:keys [start-x start-y len-x len-y]}]
-  (for [x (range len-x) y (range len-y)] [(+ start-x x) (+ start-y y)]))
+  [{:keys [x y w h]}]
+  (for [x (range x (+ x w))
+        y (range y (+ y h))]
+    [x y]))
 
 (defn get-duplicated-coords
   "claim 리스트에서 겹치는 좌표 벡터 반환
-   input: [{:start-x 1 :start-y 1 :len-x 2 :len-y 2}, {:start-x 2 :start-y 2 :len-x 2 :len-y 2}]
+   input: [{:x 1 :y 1 :w :h 2}, {:x 2 :y 2 :w 2 :h 2}]
    output: [[2 1] [2 2]]"
   [inputs]
   (->> inputs
-       (map get-coords)
-       (apply concat)
+       (mapcat get-coords)
        frequencies
        (filter #(> (second %) 1))
        (map first)))
@@ -61,31 +55,32 @@
   (->> (get-input-puzzle "day3.sample.txt")
        (map parse-input)
        (map get-claim)
-       (get-duplicated-coords)
+       get-duplicated-coords
        count))
-
 
 ;; part 2
 (defn get-coords-with-id
   "id와 해당 id의 좌표 벡터를 들고 있는 맵 생성
-   input: {:id 1 :start-x 1 :start-y 1 :len-x 2 :len-y 2}
+   input: {:id 1 :x 1 :y 1 :w 2 :h 2}
    output: {:id 1 :coords [[1 1] [1 2] [2 1] [2 2]]}"
   [input]
-  {:id (:id input) :coords (get-coords input)})
+  {:id (:id input)
+   :coords (get-coords input)})
 
-(defn is-disjoint-sets
+(defn disjoint-sets?
   "두 set이 서로소 집합인지 체크
    input: #{1 2} #{3 4}
    output: true"
   [s1 s2]
-  (= 0 (count (clojure.set/intersection s1 s2))))
+  (zero? (count (intersection s1 s2))))
 
+;; part2 무지성 풀이
 (defn solve-part2
   [inputs]
   (let [duplicated-coords (get-duplicated-coords inputs)]
     (->> inputs
          (map get-coords-with-id)
-         (filter (fn [v] (is-disjoint-sets (set (:coords v)) (set duplicated-coords))))
+         (filter (fn [v] (disjoint-sets? (set (:coords v)) (set duplicated-coords))))
          first
          :id)))
 
@@ -94,3 +89,16 @@
        (map parse-input)
        (map get-claim)
        (solve-part2)))
+
+;; part2 리팩토링
+(comment
+  (let [claims (->> (get-input-puzzle "day3.sample.txt")
+                    (map parse-input)
+                    (map get-claim))
+        duplicated-coords (get-duplicated-coords claims)]
+    (->> claims
+         (map get-coords-with-id)
+         (filter #(disjoint-sets? (set (:coords %)) (set duplicated-coords)))
+         (map #(:id %))
+         first)))
+  
