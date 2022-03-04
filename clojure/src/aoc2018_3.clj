99556c43a795922fa382ce31dc993151fbf5e7f6
SongKJ00 <gowithdsm@gmail.com>
2022-03-04 11:51:58 +0900

    day3) part2 해결

--- clojure/src/aoc2018_3.clj
+++ clojure/src/aoc2018_3.clj
@@ -1,32 +1,11 @@
 (ns aoc2018_3
   (:require [clojure.java.io :as io] [clojure.string :as str]))
 
-
-;; 파트 1
-;; 다음과 같은 입력이 주어짐.
-
-;; #1 @ 1,3: 4x4
-;; #2 @ 3,1: 4x4
-;; #3 @ 5,5: 2x2
-
-;; # 뒤에 오는 숫자는 ID, @ 뒤에 오는 숫자 쌍 (a, b)는 시작 좌표, : 뒤에 오는 (c x d)는 격자를 나타냄.
-;; 입력의 정보대로 격자 공간을 채우면 아래와 같이 됨.
-
-;;      ........
-;;      ...2222.
-;;      ...2222.
-;;      .11XX22.
-;;      .11XX22.
-;;      .111133.
-;;      .111133.
-;;      ........
-
-;; 여기서 XX는 ID 1, 2, 3의 영역이 두번 이상 겹치는 지역.
-;; 겹치는 지역의 갯수를 출력하시오. (위의 예시에서는 4)
-(defn get-input-puzzle
-  [filename]
-  (let [input-file (io/resource filename)]
-    (str/split (slurp input-file) #"\n")))
+(defn get-input-puzzle [filename]
+  (-> filename
+      io/resource
+      slurp
+      (str/split #"\n")))
 
 (defn parse-inputs
   [inputs]
@@ -37,22 +16,46 @@
      :len-x (nth blocks 3),
      :len-y (nth blocks 4)}))
 
-(defn get-used-coords
+;; part 1
+(defn get-coords
   [{:keys [start-x start-y len-x len-y]}]
   (for [x (range len-x) y (range len-y)] [(+ start-x x) (+ start-y y)]))
+
+(defn get-duplicated-coords
+  [inputs]
+  (->> inputs
+       (map get-coords)
+       (apply concat)
+       frequencies
+       (filter #(> (second %) 1))
+       (map first)))
 
 (comment
   (->> (get-input-puzzle "day3.sample.txt")
        (map parse-inputs)
-       (map get-used-coords)
-       (apply concat)
-       frequencies
-       vals
-       (filter #(> % 1))
+       (get-duplicated-coords)
        count))
 
 
-;; 파트 2
-;; 입력대로 모든 격자를 채우고 나면, 정확히 한 ID에 해당하는 영역이 다른 어떤 영역과도 겹치지 않음
-;; 위의 예시에서는 ID 3 이 ID 1, 2와 겹치지 않음. 3을 출력.
-;; 겹치지 않는 영역을 가진 ID를 출력하시오. (문제에서 답이 하나만 나옴을 보장함)
+;; part 2
+(defn get-coords-with-id
+  [input]
+  {:id (:id input) :coords (get-coords input)})
+
+(defn has-intersection
+  [s1 s2]
+  (= 0 (count (clojure.set/intersection s1 s2))))
+
+(defn solve-part2
+  [inputs]
+  (let [duplicated-coords (get-duplicated-coords inputs)]
+    (->> inputs
+         (map get-coords-with-id)
+         (filter (fn [v] (has-intersection (set (:coords v)) (set duplicated-coords))))
+         first
+         :id)))
+
+(comment
+  (->> (get-input-puzzle "day3.sample.txt")
+       (map parse-inputs)
+       (solve-part2)))
