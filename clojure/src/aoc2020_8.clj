1eda94788d0aaa6ba09afd24a5ad14bf48442962
SongKJ00 <gowithdsm@gmail.com>
2022-03-11 20:18:48 +0900

    2020-day8) 리뷰 내용 리팩토링

--- clojure/src/aoc2020_8.clj
+++ clojure/src/aoc2020_8.clj
@@ -20,86 +20,102 @@
                {:op op
                 :arg (Integer/parseInt arg)}))))
 
-(defn execute-instruction
+
+;; case로 구현한 것 -> multimethod로 바꿔보기
+(defmulti execute-instruction
   "명령어를 실행하여 다음에 실행할 명령어 idx, acc, 실행했던 명령어 idx들을 업데이트한 새로운 state를 반환
-   input: {:instruction-idx 0
+   input: {:idx 0
            :acc 0
-           :executed-instruction-idxs #{}
+           :executed-idxs #{}
            :instructions [{:op acc :arg 3} {:op jmp :arg 4}]}
-   output: {:instruction-idx 1
+   output: {:idx 1
             :acc 3
-            :executed-instruction-idxs #{0}
+            :executed-idxs #{0}
             :instructions [{:op acc :arg 3} {:op jmp :arg 4}]}
    
-   input: {:instruction-idx 1
+   input: {:idx 1
            :acc 3
-           :executed-instruction-idxs #{0}
+           :executed-idxs #{0}
            :instructions [{:op acc :arg 3} {:op jmp :arg 4}]}
-   output: {:instruction-idx 5
+   output: {:idx 5
             :acc 3
-            :executed-instruction-idxs #{0 1}
+            :executed-idxs #{0 1}
             :instructions [{:op acc :arg 3} {:op jmp :arg 4}]}"
-  [{:keys [instruction-idx acc executed-instruction-idxs instructions] :as state}]
-  (let [curr-instruction (nth instructions instruction-idx)
-        {op :op arg :arg} curr-instruction]
-    (case op
-      "acc" (assoc state
-                   :instruction-idx (inc instruction-idx)
-                   :acc (+ acc arg)
-                   :executed-instruction-idxs (conj executed-instruction-idxs instruction-idx))
-      "jmp" (assoc state
-                   :instruction-idx (+ instruction-idx arg)
-                   :executed-instruction-idxs (conj executed-instruction-idxs instruction-idx))
-      "nop" (assoc state
-                   :instruction-idx (inc instruction-idx)
-                   :executed-instruction-idxs (conj executed-instruction-idxs instruction-idx)))))
+  (fn [{:keys [idx instructions]}]
+    ((nth instructions idx) :op)))
+
+(defmethod execute-instruction "acc"
+  [{:keys [idx acc executed-idxs instructions] :as state}]
+  (let [{arg :arg} (nth instructions idx)]
+    (assoc state
+           :idx (inc idx)
+           :acc (+ acc arg)
+           :executed-idxs (conj executed-idxs idx))))
+
+(defmethod execute-instruction "jmp"
+  [{:keys [idx executed-idxs instructions] :as state}]
+  (let [{arg :arg} (nth instructions idx)]
+    (assoc state
+           :idx (+ idx arg)
+           :executed-idxs (conj executed-idxs idx))))
+
+(defmethod execute-instruction "nop"
+  [{:keys [idx executed-idxs instructions] :as state}]
+  (let [{arg :arg} (nth instructions idx)]
+    (assoc state
+           :idx (inc idx)
+           :executed-idxs (conj executed-idxs idx))))
 
 (defn update-running-state
   "현재 state를 확인하여 running-state 값이 업데이트된 새로운 state 반환
-   1) 다음에 실행할 명령어 idx가 실행했던 명령어 idx에 이미 있는 경우 -> in loop로 변경
-   input: {:instruction-idx 0
+   1) 다음에 실행할 명령어 idx가 실행했던 명령어 idx에 이미 있는 경우 -> in-loop로 변경
+   input: {:idx 0
            :acc 3
-           :executed-instruction-idxs #{0 1}
+           :executed-idxs #{0 1}
            :instructions [{:op acc :arg 3} {:op jmp :arg -1}]
-           :running-state \"running\"}
-   output: {:instruction-idx 0
-           :acc 3
-           :executed-instruction-idxs #{0 1}
-           :instructions [{:op acc :arg 3} {:op jmp :arg -1}]
-           :running-state \"in loop\"}
+           :running-state :running}
+   output: {:idx 0
+            :acc 3
+            :executed-idxs #{0 1}
+            :instructions [{:op acc :arg 3} {:op jmp :arg -1}]
+            :running-state :in-loop}
    
    2) 다음에 실행할 명령어 idx가 전체 명령어 길이를 넘는 경우 -> terminated로 변경
-   input: {:instruction-idx 5
+   input: {:idx 5
            :acc 3
-           :executed-instruction-idxs #{1 2}
+           :executed-idxs #{1 2}
            :instructions [{:op acc :arg 3} {:op jmp :arg 4}]
-           :running-state \"running\"}
-   output: {:instruction-idx 5
+           :running-state :running}
+   output: {:idx 5
            :acc 3
-           :executed-instruction-idxs #{1 2}
+           :executed-idxs #{1 2}
            :instructions [{:op acc :arg 3} {:op jmp :arg 4}]
-           :running-state \"terminated\"}
+           :running-state :terminated}
            
    3) 그 이외 경우 -> running으로 유지"
-  [{:keys [instruction-idx executed-instruction-idxs instructions] :as state}]
+  [{:keys [idx executed-idxs instructions] :as state}]
   (cond
-    (executed-instruction-idxs instruction-idx) (assoc state :running-state "in loop")
-    (>= instruction-idx (count instructions)) (assoc state :running-state "terminated")
-    :else (assoc state :running-state "running")))
+    (executed-idxs idx)
+    (assoc state :running-state :in-loop)
+
+    (>= idx (count instructions))
+    (assoc state :running-state :terminated)
+
+    :else (assoc state :running-state :running)))
 
 (defn get-next-state
   "명령어 하나를 실행하여 다음 state를 얻는 함수
    명령어 실행 결과 이외에도 다음 명령어를 실행할 수 있는지에 대한 여부(running-state)도 같이 업데이트
-   input: {:instruction-idx 0
+   input: {:idx 0
            :acc 0
-           :executed-instruction-idxs #{}
+           :executed-idxs #{}
            :instructions [{:op acc :arg 3} {:op jmp :arg 4}]
-           :running-state \"running\"}
-   output: {:instruction-idx 1
+           :running-state :running}
+   output: {:idx 1
             :acc 3
-            :executed-instruction-idxs #{0}
+            :executed-idxs #{0}
             :instructions [{:op acc :arg 3} {:op jmp :arg 4}]
-            :running-state \"running\"}"
+            :running-state :running}"
   [state]
   (-> state
       execute-instruction
@@ -108,28 +124,21 @@
 (defn get-first-state-not-running
   "running-state가 처음으로 running이 아닌 state를 찾아 반환
    input: [{:op acc :arg 3} {:op jmp :arg 1} {:op jmp :arg -2}]
-   output: {:instruction-idx 0
+   output: {:idx 0
             :acc 3
-            :executed-instruction-idxs #{0 1 2}
+            :executed-idxs #{0 1 2}
             :instructions [{:op acc :arg 3} {:op jmp :arg 1} {:op jmp :arg -2}]
-            :running-state \"in loop\"}"
+            :running-state :in-loop}"
   [instructions]
-  (let [initial-state {:instruction-idx 0
+  (let [initial-state {:idx 0
                        :acc 0
-                       :executed-instruction-idxs #{}
+                       :executed-idxs #{}
                        :instructions instructions
-                       :running-state "running"}]
+                       :running-state :running}]
     (->> initial-state
          (iterate get-next-state)
-         (drop-while #(= (% :running-state) "running"))
+         (drop-while #(= (% :running-state) :running)) ;; keyword로 관ㄹ
          first)))
-
-(comment
-  (let [inputs (get-input-puzzle "2020/day8.sample.txt")
-        instructions (inputs->instructions inputs)]
-    (-> instructions
-        get-first-state-not-running
-        :acc)))
 
 (defn find-jmp-or-nop-idxs
   "명령어 벡터에서 op값이 jmp 혹은 nop인 명령어의 인덱스들을 반환
@@ -156,11 +165,20 @@
                     "nop" (assoc instructions idx {:op "jmp" :arg arg}))))))))
 
 (comment
+  ;; part1
+  (let [inputs (get-input-puzzle "2020/day8.sample.txt")
+        instructions (inputs->instructions inputs)]
+    (-> instructions
+        get-first-state-not-running
+        :acc))
+  
+  ;; part2
   (let [inputs (get-input-puzzle "2020/day8.sample.txt")
         instructions (inputs->instructions inputs)
         jmp<->nop-once-instructions (jmp<->nop-once instructions)]
     (->> jmp<->nop-once-instructions
          (map get-first-state-not-running)
-         (filter #(= (% :running-state) "terminated"))
+         (filter #(= (% :running-state) :terminated))
          first
-         :acc)))
+         :acc))
+  )
